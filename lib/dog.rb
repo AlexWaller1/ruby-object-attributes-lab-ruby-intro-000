885d415d94a27a09e8d694bb383984e60f943a97
grateful-closure-7122 <alex@artofalexwaller.com>
2020-08-25 06:21:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
-  def name = (dog_name)
-    @name = dog_name
+  def name=(dog_name)
+    @name=dog_name
   end
 
   def name
     @name
   end
 
-  def breed = (breed)
-    @breed = breed
+  def breed=(breed)
+    @breed=breed
   end
 
   def breed
